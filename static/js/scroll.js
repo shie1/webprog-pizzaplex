293a97aa04ef864d1c521e9af1fe5f6318770ca7
Shie1 <sonkolyw@gmail.com>
2023-12-16 22:23:37 +0100

    reserve space for scrollbar

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,7 +1,11 @@
 const hideScrollbar = () => {
     document.body.style.overflow = 'hidden';
+    // reserve space for scrollbar
+    document.body.style.paddingRight = '5px';
 };
 
 const showScrollbar = () => {
     document.body.style.overflow = 'auto';
+    document.body.style.paddingRight = '0';
 }  
+
